98a7df97652701628384d8d839c7ba2fcff3e132
Zhongyan Luo <zhongyan_luo@ucsb.edu>
2025-04-17 02:09:03 -0700

    update

--- global.js
+++ global.js
@@ -1,7 +1,7 @@
 console.log('IT’S ALIVE!');
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-  ? "/" : "/zhongyan0721.github.io/portfolio/";
+  ? "/" : "/portfolio/";
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
