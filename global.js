ce63b49b3b9653d5c20daf24b6f0583c57c87501
Zhongyan Luo <zhongyan_luo@ucsb.edu>
2025-04-16 20:51:12 -0700

    update

--- global.js
+++ global.js
@@ -1,14 +1,17 @@
 console.log('IT’S ALIVE!');
 
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/" : "/zhongyan0721.github.io/";
+
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a")
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-  );
-currentLink?.classList.add('current');
+
+
+
+
+
 
 let pages = [
     { url: '', title: 'Home' },
@@ -21,6 +24,13 @@ let nav = document.createElement('nav');
 document.body.prepend(nav);
 for (let p of pages) {
     let url = p.url;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
     let title = p.title;
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+      }
   }
