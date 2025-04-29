90d38f5cfcf6b34a9a7bd717739400c8e8108369
Zhongyan Luo <zhongyan_luo@ucsb.edu>
2025-04-29 06:30:52 -0700

    update

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
@@ -9,4 +10,39 @@ const jsElements = document.getElementsByClassName('project-title');
 const count = jsElements.length;
 document.querySelector('h1').insertAdjacentHTML('afterbegin', count + ' ');
 
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
+let data = [
+    { value: 1, label: 'apples' },
+    { value: 2, label: 'oranges' },
+    { value: 3, label: 'mangos' },
+    { value: 4, label: 'pears' },
+    { value: 5, label: 'limes' },
+    { value: 5, label: 'cherries' },
+  ];
+  
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+let sliceGenerator = d3.pie().value((d) => d.value);
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+let total = 0;
+
+for (let d of data) {
+  total += d;
+}
+
+let angle = 0;
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+arcs.forEach((arc, idx) => {
+    d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', colors(idx))
+})
